99994f72faad8c679c250436c1f96b08419be65a
jDan735 <daniel734@bk.ru>
2020-07-06 20:30:50 +0000

    Print version

--- app.py
+++ app.py
@@ -5,7 +5,12 @@ import design
 class QtTwitter(QtWidgets.QMainWindow, design.Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        self.setupUi(self) 
+        self.setupUi(self)
+        
+        self.actionAbout_QtTwitter.triggered.connect(self.about_app)
+        
+    def about_app(self):
+    	print("QtTwitter v0.1.2")
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
